3b177bf7e8edf5912f82f2a96937c37f6623b4b7
dsantos-emps <david.santos@eagles.oc.edu>
2024-10-24 18:57:48 -0500

    Initial setup

---
+++ resume-builder/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-app-bar>
+        <v-toolbar-title>ResumeBuilder  </v-toolbar-title>
+        <v-btn text to="/">Home</v-btn>
+        <v-spacer></v-spacer>
+        <v-btn text to="/login">Login</v-btn>
+      </v-app-bar>
+
+      <router-view />
+    </v-main>
+  </v-app>
+</template>
+
+<script setup>
+//
+</script>
