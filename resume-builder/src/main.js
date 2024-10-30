a4b40741271d253c0f999d848674a4894f46359e
PrasmitP <p.pokhrel@eagles.oc.edu>
2024-10-30 12:59:06 -0500

    Login page button (not fully working yet)

--- resume-builder/src/main.js
+++ resume-builder/src/main.js
@@ -8,6 +8,7 @@
 import { registerPlugins } from '@/plugins'
 
 // Components
+import Vue from 'vue'
 import App from './App.vue'
 
 // Composables
@@ -20,3 +21,19 @@ const app = createApp(App).use(createPinia())
 registerPlugins(app)
 
 app.mount('#app')
+
+
+import GoogleAuth from '@/config/google_oAuth.js'
+const gauthOption = {
+  clientId: '209811569480-gob89jfqps61m9nsiv0gm53n3n4g89u1.apps.googleusercontent.com',
+  scope: 'profile email',
+  prompt: 'select_account'
+}
+Vue.use(GoogleAuth, gauthOption)
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
