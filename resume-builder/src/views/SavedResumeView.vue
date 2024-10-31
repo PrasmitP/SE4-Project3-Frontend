6d1c3e4a121d8a6c5b95eddc722d4f490b72eb4b
dsantos-emps <david.santos@eagles.oc.edu>
2024-10-31 03:44:29 -0500

    Added SavedResumeView

---
+++ resume-builder/src/views/SavedResumeView.vue
@@ -0,0 +1,22 @@
+<template>
+    <v-container>
+        <h1>Your resume has been saved</h1>
+        <div class="resumePreview"></div>
+        <h2>Ai Suggestions</h2>
+        <p>ai suggestion text should go here</p>
+        <v-btn>Edit</v-btn>
+        <v-btn>Download PDF</v-btn>
+    </v-container>
+</template>
+<script setup></script>
+<style scoped>
+.resumePreview{
+    border: 1px solid black;
+    background-color: white;
+    height: 1000px;
+    width: 800px;
+    margin: 0 auto;
+    margin-top: 20px;
+    margin-bottom: 20px;
+}
+</style>
