5d02ca7f874f3dd82dc0e9c7935726db1a624d62
dsantos-emps <david.santos@eagles.oc.edu>
2024-10-31 17:24:05 -0500

    Add My Information route, ResumePreview component, and enhance IconRow functionality

--- resume-builder/src/router/index.js
+++ resume-builder/src/router/index.js
@@ -4,6 +4,8 @@ import HomeView from "../views/HomeView.vue";
 import BuilderView from "../views/BuilderView.vue";
 import LoginView from "../views/LoginView.vue";
 import SavedResumeView from "@/views/SavedResumeView.vue";
+import MyInfoView from "@/views/MyInfoView.vue";
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -24,6 +26,12 @@ const router = createRouter({
       path:"/build/saved", // this is a route for saved resumes, should probably be changed later
       name: "saved",
       component: SavedResumeView,
+      props: true,
+    },
+    {
+      path: "/myInfo",
+      name: "myInfo",
+      component: MyInfoView,
       props: true,
     },
     {
